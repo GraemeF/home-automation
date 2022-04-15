184012c72a42de24b572bdc2f04afdf032be48a3
Graeme Foster <graeme@codeforbreakfast.co>
2022-04-15 15:37:40 +0100

    Read config from a file

--- packages/deep-heating-socketio/src/main.ts
+++ packages/deep-heating-socketio/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import { readFileSync } from 'fs';
+import { createServer } from 'http';
+import { SocketServer } from './app/socket-server';
+
+const data = readFileSync(
+  process.env['DEEP_HEATING_HOME_CONFIG_FILE'] || './home.json'
+);
+
+const httpServer = createServer();
+
+new SocketServer(httpServer, JSON.parse(data.toString()));
+
+httpServer.listen(3000);
