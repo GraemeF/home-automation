42bb76c4bc1334eb670ace5b8ad7e57efa1e6fbd
Graeme Foster <graeme@codeforbreakfast.co>
2022-04-16 18:55:23 +0100

    Build a docker image for deep-heating-socketio

--- packages/deep-heating-socketio/src/main.ts
+++ packages/deep-heating-socketio/src/main.ts
@@ -5,6 +5,9 @@ config();
 import { readFileSync } from 'fs';
 import { createServer } from 'http';
 import { SocketServer } from './app/socket-server';
+import debug from 'debug';
+
+const log = debug('app');
 
 const data = readFileSync(
   process.env['DEEP_HEATING_HOME_CONFIG_FILE'] || './home.json'
@@ -12,6 +15,23 @@ const data = readFileSync(
 
 const httpServer = createServer();
 
-new SocketServer(httpServer, JSON.parse(data.toString()));
+const socketServer = new SocketServer(httpServer, JSON.parse(data.toString()));
 
-httpServer.listen();
+process.on('SIGINT', function () {
+  log('Shutting down');
+  socketServer.dispose();
+  httpServer.close();
+  process.exit();
+});
+
+process.on('SIGTERM', function () {
+  log('Terminating');
+  socketServer.dispose();
+  httpServer.close();
+  process.exit();
+});
+
+const port = process.env['PORT'] || 5123;
+log('Listening on port %d', port);
+
+httpServer.listen(port);
