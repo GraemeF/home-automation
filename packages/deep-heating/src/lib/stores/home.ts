e657f38642f7dee4aeb8b4cf2de6681eb10f6dd6
Graeme Foster <graeme@codeforbreakfast.co>
2022-04-17 14:11:04 +0100

    Show when connecting

--- packages/deep-heating/src/lib/stores/home.ts
+++ packages/deep-heating/src/lib/stores/home.ts
@@ -1,23 +1,39 @@
-import { derived } from 'svelte/store';
+import { derived, writable, get } from 'svelte/store';
 import type { Readable } from 'svelte/store';
 import { apiClientStore } from './apiClient';
 import type { Socket } from 'socket.io-client';
 import type { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import type { DeepHeatingState } from '@home-automation/deep-heating-types';
 
+interface Home {
+  connected: boolean;
+  state: DeepHeatingState | null;
+}
+
 export const homeStore = derived<
   Readable<Socket<DefaultEventsMap, DefaultEventsMap>>,
-  DeepHeatingState | undefined
->(apiClientStore, ($apiClient, set) => {
-  $apiClient.on('connect', () => {
-    console.log('connected');
-  });
+  Home
+>(
+  apiClientStore,
+  ($apiClient, set) => {
+    const home = writable<Home>({
+      connected: false,
+      state: null,
+    });
 
-  $apiClient.on('disconnect', () => {
-    console.log('disconnected');
-  });
-
-  $apiClient.on('State', (state) => {
-    set(state);
-  });
-});
+    $apiClient
+      .on('connect', () => {
+        home.update((home) => ({ ...home, connected: true }));
+        set(get(home));
+      })
+      .on('disconnect', () => {
+        home.update((home) => ({ ...home, connected: false }));
+        set(get(home));
+      })
+      .on('State', (state) => {
+        home.update((home) => ({ ...home, state }));
+        set(get(home));
+      });
+  },
+  { connected: false, state: null }
+);
--- packages/deep-heating/src/routes/index.svelte
+++ packages/deep-heating/src/routes/index.svelte
@@ -1,5 +1,17 @@
 <script>
   import { homeStore } from '$lib/stores/home';
+  import Spinner from '$lib/components/Spinner.svelte';
 </script>
 
-<pre>{JSON.stringify($homeStore, null, 2)}</pre>
+<h1>Deep Heating</h1>
+{#if $homeStore.connected}
+  <pre>{JSON.stringify($homeStore, null, 2)}</pre>
+{:else}
+  <div class="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
+    <span
+      class="opacity-75 top-1/2 mx-auto relative flex items-center justify-center"
+    >
+      <Spinner />
+    </span>
+  </div>
+{/if}
