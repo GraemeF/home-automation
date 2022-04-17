db52a6fc41455c06ffe202319b8e9962f5f2edf4
Graeme Foster <graeme@codeforbreakfast.co>
2022-04-17 14:47:50 +0100

    Show a stat per room

---
+++ packages/deep-heating/src/lib/components/Room.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { RoomState } from '@home-automation/deep-heating-types';
+
+  export let room: RoomState;
+</script>
+
+<div
+  class="stat  text-primary-content"
+  class:bg-heating={room.isHeating}
+  class:bg-cooling={!room.isHeating}
+>
+  <div class="stat-title">{room.name}</div>
+  <div class="stat-value">{room.temperature.temperature}</div>
+  <div class="stat-unit">°C</div>
+  <div class="stat-desc">{room.adjustment}</div>
+</div>
--- packages/deep-heating/src/routes/index.svelte
+++ packages/deep-heating/src/routes/index.svelte
@@ -2,11 +2,18 @@
   import { homeStore } from '$lib/stores/home';
   import Spinner from '$lib/components/Spinner.svelte';
   import Heating from '$lib/components/Heating.svelte';
+  import Room from '$lib/components/Room.svelte';
 </script>
 
 {#if $homeStore.connected}
   <h1>Deep Heating</h1>
   <Heating isHeating={$homeStore.state.isHeating} />
+
+  <div class="stats shadow stats-vertical lg:stats-horizontal">
+    {#each $homeStore.state.rooms.sort((a, b) => (a.temperature?.temperature ?? 999) - (b.temperature?.temperature ?? 999)) as room}
+      <Room {room} />
+    {/each}
+  </div>
   <pre>{JSON.stringify($homeStore, null, 2)}</pre>
 {:else}
   <div class="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
