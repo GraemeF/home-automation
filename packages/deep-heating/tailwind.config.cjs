4fa1887efa056e16ed0e920a5743b3e7ca4474fd
Graeme Foster <graeme@codeforbreakfast.co>
2022-04-17 14:46:53 +0100

    Format

--- packages/deep-heating/tailwind.config.cjs
+++ packages/deep-heating/tailwind.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    colors:{
-      'heating':'#FF9700',
-      'cooling':'#77DAE8',
+    colors: {
+      heating: '#FF9700',
+      cooling: '#77DAE8',
     },
     extend: {},
   },
