18cba93fba9abf6cb1765db1d32e929bb47e914a
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2023-04-23 19:24:31 +0100

    fix(build): disable noExternal

--- packages/deep-heating-web/vite.config.js
+++ packages/deep-heating-web/vite.config.js
@@ -3,7 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  ssr: process.env.NODE_ENV === 'production' ? { noExternal: true } : undefined,
+  ssr: process.env.NODE_ENV === 'production' ? {} : undefined,
   server: { fs: { strict: process.env.NODE_ENV === 'production' } },
 };
 
