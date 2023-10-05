cdde120fa5034346ee5b4fa8264d29778c00c880
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2023-10-05 08:41:28 +0100

    Clean up

--- packages/deep-heating-home-assistant/src/lib/home-assistant.ts
+++ packages/deep-heating-home-assistant/src/lib/home-assistant.ts
@@ -1,10 +1,6 @@
 import { from, Observable, timer } from 'rxjs';
 import { getClimateEntities } from './home-assistant-api';
 import { shareReplay, switchMap, throttleTime } from 'rxjs/operators';
-import {
-  TemperatureReading,
-  TrvModeValue,
-} from '@home-automation/deep-heating-types';
 import { ClimateEntity } from './schema';
 import { Effect, pipe } from 'effect';
 
@@ -16,63 +12,3 @@ export const getClimateEntityUpdates = (): Observable<ClimateEntity> =>
     switchMap(() => from(pipe(getClimateEntities, Effect.runSync))),
     shareReplay(1)
   );
-
-export interface HomeAssistantHeatingScheduleSlot {
-  value: {
-    heat?: number;
-    target?: number;
-  };
-  start: number;
-}
-
-export type HomeAssistantDayHeatingSchedule =
-  HomeAssistantHeatingScheduleSlot[];
-
-export interface HomeAssistantHeatingSchedule {
-  monday: HomeAssistantDayHeatingSchedule;
-  tuesday: HomeAssistantDayHeatingSchedule;
-  wednesday: HomeAssistantDayHeatingSchedule;
-  thursday: HomeAssistantDayHeatingSchedule;
-  friday: HomeAssistantDayHeatingSchedule;
-  saturday: HomeAssistantDayHeatingSchedule;
-  sunday: HomeAssistantDayHeatingSchedule;
-}
-
-export interface TrvUpdate {
-  state: {
-    temperature: TemperatureReading;
-    target: number;
-    mode: TrvModeValue;
-    isHeating: boolean;
-    schedule: HomeAssistantHeatingSchedule;
-  };
-  trvId: string;
-  deviceType: string;
-  name: string;
-}
-
-export interface HeatingUpdate {
-  state: {
-    temperature: TemperatureReading;
-    target: number;
-    mode: string;
-    isHeating: boolean;
-    schedule: HomeAssistantHeatingSchedule;
-  };
-  heatingId: string;
-  name: string;
-}
-
-export interface TrvControlState {
-  trvId: string;
-  targetTemperature: number;
-  mode: TrvModeValue;
-  source: HomeAssistantUpdateSource;
-}
-
-export type HomeAssistantUpdateSource = 'HomeAssistant' | 'Synthesised';
-
-export interface RoomHomeAssistantHeatingSchedule {
-  roomName: string;
-  schedule: HomeAssistantHeatingSchedule;
-}
