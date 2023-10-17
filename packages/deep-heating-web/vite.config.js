40ad5ecd3bbe8204d520859085f2c01b60ab0eb9
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2023-10-17 18:27:06 +0100

    chore: add vite checker

--- packages/deep-heating-web/vite.config.js
+++ packages/deep-heating-web/vite.config.js
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import checker from 'vite-plugin-checker';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), checker({ typescript: true })],
   ssr: process.env.NODE_ENV === 'production' ? {} : undefined,
   server: { fs: { strict: process.env.NODE_ENV === 'production' } },
 };
