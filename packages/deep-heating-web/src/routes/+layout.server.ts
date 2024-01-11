364f78089ab280d781f27a2a975c6b8a24b8ba8a
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2024-01-11 11:12:08 +0000

    fix(addon): try using X-Ingress-Path to find api

--- packages/deep-heating-web/src/routes/+layout.server.ts
+++ packages/deep-heating-web/src/routes/+layout.server.ts
@@ -1,6 +1,23 @@
 import type { AppSettings } from '$lib/stores/appsettings';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad<AppSettings> = () => ({
-  apiUrl: process.env['API_URL'],
-});
+const getApiUrl = (request: Request) => {
+  if (process.env['API_URL']) {
+    console.log('Using API_URL', process.env['API_URL']);
+    return process.env['API_URL'];
+  }
+
+  if (request.headers.get('X-Ingress-Path')) {
+    console.log('Using X-Ingress-Path', request.headers.get('X-Ingress-Path'));
+    return request.headers.get('X-Ingress-Path') ?? undefined;
+  }
+
+  console.log('No API URL specified');
+  return undefined;
+};
+
+export const load: LayoutServerLoad<AppSettings> = ({ request }) => {
+  return {
+    apiUrl: getApiUrl(request),
+  };
+};
