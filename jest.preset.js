2ae8b593e9c1fb4294d78aa6b0138e4252536b6f
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-09-30 14:58:47 +0100

    fix: remove @nx/jest preset and improve turbo task dependencies
    - Removed @nx/jest/preset dependency from jest.preset.js
    - Added moduleNameMapper to resolve workspace dependencies in tests
    - Removed test scripts from packages without tests (deep-heating-state, dictionary, rxx, deep-heating-socketio)
    - Made lint depend on format to ensure formatting is done first
    - Made test depend on lint to ensure linting passes before tests
    - Made dev/serve depend on ^build to ensure dependencies are built first
    - All tests now passing

--- jest.preset.js
+++ jest.preset.js
@@ -1,5 +1,8 @@
-const nxPreset = require('@nx/jest/preset').default;
-
 module.exports = {
-  ...nxPreset,
+  moduleNameMapper: {
+    '^@home-automation/(.*)$': '<rootDir>/../../packages/$1/src',
+    '^deep-heating-(.*)$': '<rootDir>/../../packages/deep-heating-$1/src',
+    '^dictionary$': '<rootDir>/../../packages/dictionary/src',
+    '^rxx$': '<rootDir>/../../packages/rxx/src',
+  },
 };
