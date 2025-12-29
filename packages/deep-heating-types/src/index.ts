8a025cd3adba716771d37ae40cf09c1939edf539
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-12-29 18:37:37 +0000

    feat(deep-heating-types): implement InMemoryHeatingSystem for testing (#1314)

--- packages/deep-heating-types/src/index.ts
+++ packages/deep-heating-types/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/entities';
 export * from './lib/heatingActions';
 export * from './lib/heatingProvider';
 export * from './lib/heatingSystem';
+export * from './lib/inMemoryHeatingSystem';
 export * from './lib/home-assistant';
 export * from './lib/schedule-types';
 export * from './lib/temperature';
